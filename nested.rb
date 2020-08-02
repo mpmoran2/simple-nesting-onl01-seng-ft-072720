7d5f675c1959925fe0ab4f99a9da88459f76797a
Megan (AKA Pilahi) Moran <kamokumoran@gmail.com>
2020-08-02 00:48:23 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -105,7 +105,8 @@ def changing_alan
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
      
-     
+     programmer_hash[:alan_kay][:known_for] = alans_new_info
+     return programmer_hash
 end
 
 def adding_to_dennis
@@ -124,5 +125,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-
+    programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+    return programmer_hash
 end
